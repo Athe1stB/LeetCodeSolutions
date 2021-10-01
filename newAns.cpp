fbc4b7402d5df7a8edaabe7620af9a2ef11b37da
PervySag3 <91727492+PervySag3@users.noreply.github.com>
2021-10-01 14:54:15 +0530

    Create newAns.cpp

---
+++ newAns.cpp
@@ -0,0 +1,109 @@
+#include<bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+#define ll long long
+#define lld long double
+#define ull unsigned long long
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define forA(i,a,n,x) for(ll i=a;i<n;i+=x)
+#define forD(i,a,n,x) for(ll i=n-1;i>=a;i-=x)
+#define ranit(it,n) for(auto &it:n)
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+#define mod 1000000007
+#define maxheap priority_queue<ll>
+#define minheap priority_queue<ll, vll , greater<ll>>
+#define inf LLONG_MAX-1
+#define init(a,x) memset(a,x,sizeof(a))
+#define fastio() ios_base::sync_with_stdio(false);    cin.tie(NULL);    cout.tie(NULL)
+#define nline "\n"
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifdef Athe1stB
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+ll tno = 1;
+// const ll x[4] = {0, 1, 0, -1};
+// const ll y[4] = {-1, 0, 1, 0};
+// const ll x[8] = {-1, 0, 1, 1, 1, 0, -1, -1};
+// const ll y[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
+
+ll sieveSize = 100003;
+vll sv(sieveSize), primes;
+void sieve()
+{
+    sv[2]=1; primes.pb(2);
+    forA(i,3,sieveSize,2)sv[i]=1;
+    forA(i,3,sieveSize,2)
+        if(sv[i]==1)
+        {
+            primes.pb(i);
+            forA(j,i*i,sieveSize,i) sv[j]=0;
+        }
+}
+
+ll solve()
+{
+    cerr<<"\nCase #"<<tno<<":"<<"\n";
+
+    ll x;
+    cin>>x;
+
+    auto lb = lower_bound(all(primes),x);
+    if(*lb==x)
+        return 0;
+
+    if(lb == primes.begin())
+        return x;
+
+    lb--;
+    return (x-(*lb));
+
+    cerr<<"\n";
+}
+
+int main()
+{
+    fastio();
+    auto start1 = high_resolution_clock::now();
+    ll t=1;
+    cin>>t;
+    sieve();
+    while(t--)
+        cout<<solve()<<nline,tno++;
+    auto stop1 = high_resolution_clock::now();
+    duration<double, milli> ms = stop1-start1;
+        
+    #ifdef Athe1stB
+        cerr<<"Finished in "<< ms.count()<< " ms\n";
+    #endif
+        
+    return 0;
+}
